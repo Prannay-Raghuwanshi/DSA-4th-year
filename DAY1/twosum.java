732337ffc1ee2bfc86c3824a79b1681dfca22d6a
Prannay-Raghuwanshi <156276494+Prannay-Raghuwanshi@users.noreply.github.com>
2026-07-01 22:29:06 +0530

    Add files via upload

---
+++ DAY1/twosum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       HashMap<Integer,Integer>map = new HashMap<>();
+       int[] ans={-1,-1};
+       for(int i=0;i<nums.length;i++){
+        int diff=target-nums[i];
+        if(!map.containsKey(diff)){
+            map.put(nums[i],i);
+        }
+         else{
+            ans[0]=i;
+            ans[1]=map.get(diff);
+            return ans;
+         }
+        }
+        return ans;
+       } 
+    }
